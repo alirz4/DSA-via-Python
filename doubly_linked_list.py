530255a1039abf5e027cf165b3b804569dbb3956
alirz4 <alirz4keshavrz@gmail.com>
2026-05-19 16:04:43 +0200

    constructing doubly-linked-list and some of its methods

---
+++ doubly_linked_list.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedListNode:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return False
+        tmp = self.tail 
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            tmp.prev = None
+        self.length -= 1
+        return tmp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+        
+    def pop_first(self):
+        if self.length == 0:
+            return False
+        
+        tmp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            tmp.next = None
+            self.head.prev = None
+        self.length -= 1
+        return tmp
+
