02048b68f49ece0061c556cfa9d88a66ddf0ff67
alirz4 <alirz4keshavrz@gmail.com>
2026-05-27 20:14:44 +0200

    completing stack init and add print method

--- stack.py
+++ stack.py
@@ -1,8 +1,22 @@
+# Implementing Stack using linked-list
+
 class Node:
     def __init__(self, value):
-        pass
+        self.value = value
+        self.next = None
 
 class Stack:
     def __init__(self, value):
-        pass
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
 
+    def print_stack(self):
+        tmp = self.top
+        while tmp is not None:
+            print(tmp.value)
+            tmp = tmp.next
+
+my_stack = Stack(1)
+
+my_stack.print_stack()
